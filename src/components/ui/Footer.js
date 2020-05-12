ae74cc6e4d257422172d1236b6104f75c695f602
peiyao <peiyaochang@qnap.com>
2020-05-12 20:04:02 +0800

    Implemented the Grid and navigation

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 import footerAdornment from '../../assets/Footer Adornment.svg';
 
@@ -20,6 +22,19 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.down("xs")]: {
       width: "15em"
     }
+  },
+  mainContainer: {
+    position: "absolute"
+  },
+  link: {
+    color: "white",
+    fontFamily: "Arial",
+    fontSize: "0.75rem",
+    fontWeight: "bold",
+    textDecoration: "none",   // 移除 a link 的底線
+  },
+  gridItem: {
+    margin: "3em"
   }
 }));
 
@@ -28,6 +43,67 @@ export default function Footer() {
 
   return (
     <footer className={classes.footer}>
+      <Grid container justify="center" className={classes.mainContainer}>
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item component={Link} to="/" className={classes.link}>
+              Home
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item component={Link} to="/services" className={classes.link}>
+              Services
+            </Grid>
+            <Grid item component={Link} to="/customsoftware" className={classes.link}>
+              Custom Software Development
+            </Grid>
+            <Grid item component={Link} to="/mobileapps" className={classes.link}>
+              Mobile App Development
+            </Grid>
+            <Grid item component={Link} to="/websites" className={classes.link}>
+              Website Development
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item component={Link} to="/revolution" className={classes.link}>
+              The Revolution
+            </Grid>
+            <Grid item component={Link} to="/revolution" className={classes.link}>
+              Vision
+            </Grid>
+            <Grid item component={Link} to="/revolution" className={classes.link}>
+              Technology
+            </Grid>
+            <Grid item component={Link} to="/revolution" className={classes.link}>
+              Process
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item component={Link} to="/about" className={classes.link}>
+              About Us
+            </Grid>
+            <Grid item component={Link} to="/about" className={classes.link}>
+              History
+            </Grid>
+            <Grid item component={Link} to="/about" className={classes.link}>
+              Team
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item component={Link} to="/contact" className={classes.link}>
+              Contact Us
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
       <img
         alt="black decorative slash"
         src={footerAdornment} 
