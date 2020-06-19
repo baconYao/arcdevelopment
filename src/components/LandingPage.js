032f474dd4d1af93ccaf2b09177a40f51a0da02d
peiyao <peiyaochang@qnap.com>
2020-06-19 23:30:28 +0800

    Added the revolution block by using card component

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,11 +6,14 @@ import Button from '@material-ui/core/Button';
 import ButtonArrow from '../components/ui/ButtonArrow';
 import Typography from '@material-ui/core/Typography';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 import animationData from '../animations/landinganimation/data';
 import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 import mobileAppsIcon from '../assets/mobileIcon.svg'
 import websitesIcon from '../assets/websiteIcon.svg';
+import revolutionBackground from '../assets/repeatingBackground.svg';
 
 const useStyles = makeStyles(theme => ({
   animation: {
@@ -84,6 +87,28 @@ const useStyles = makeStyles(theme => ({
     marginTop: "12em",
     [theme.breakpoints.down("sm")]: {
       padding: 25
+    }
+  },
+  revolutionBackground: {
+    backgroundImage: `url(${revolutionBackground})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    width: "100%"
+  },
+  revolutionCard: {
+    position: "absolute",
+    boxShadow: theme.shadows[10],
+    borderRadius: 15,
+    padding: "10em",
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: "8em",
+      paddingBottom: "8em",
+      paddingLeft: 0,
+      paddingRight: 0,
+      borderRadius: 0,
+      width: "100%"
     }
   }
 }));
@@ -193,6 +218,31 @@ export default function LandingPage() {
           <Grid item>
             <img className={classes.icon} alt="website icon" src={websitesIcon} />
           </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Grid container style={{height: "60em", marginTop: "12em"}} alignItems="center" justify="center">
+          <Card className={classes.revolutionCard}>
+            <CardContent>
+              <Grid container direction="column" style={{ textAlign: "center"}}>
+                <Grid item>
+                  <Typography variant="h3" gutterBottom>
+                    The Revolution
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="subtitle1">
+                    Visionary insights coupled with cutting-edge technology is a recipe for revolution.
+                  </Typography>
+                  <Button variant="outlined" className={classes.learnButton}>
+                    <span style={{marginRight: 10}}>Learn More</span>
+                    <ButtonArrow width={10} height={10} fill={theme.palette.common.blue}/>
+                  </Button>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+          <div className={classes.revolutionBackground} />
         </Grid>
       </Grid>
     </Grid>
